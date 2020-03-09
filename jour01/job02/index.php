f4e78c5b6cfc876517daecfe6af6ff3f102c78bd
olivier-crozet <olivier.crozet@laplateforme.io>
2020-03-09 10:58:43 +0100

    job 02 ok

---
+++ jour01/job02/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+		<title>js</title>
+		   <script type="text/javascript"
+		   src="script.js">
+      		</script>
+	</head>
+	<body>
+	
+	</body>
+	</html>
+</DOCTYPE>
