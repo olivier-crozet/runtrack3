cba5cb0c7133e6f446ab0f1ace5d36592a56fe46
olivier-crozet <olivier.crozet@laplateforme.io>
2020-03-09 14:20:58 +0100

    job 05

---
+++ jour01/job05/script.js
@@ -0,0 +1,18 @@
+
+let joursemaines = ['lundi', 'mardi', 'mecredi', 'jeudi', 'vendredi','samedi','dimanche'];
+
+function afficherjourssemaine()
+{
+for (let i = 0; i < 7 ; i++)
+    {
+		
+		
+	console.log(joursemaines[i]);
+	
+	}
+}
+let result = afficherjourssemaine();
+
+//result.forEach(afficherjourssemaine(item, index, array) {
+ // console.log(item, index);
+//});
