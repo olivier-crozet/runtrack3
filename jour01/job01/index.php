8aa54440ea0dfcc0c280130972444f9d3e06a96c
olivier-crozet <olivier.crozet@laplateforme.io>
2020-03-09 10:43:01 +0100

    job01 ok

---
+++ jour01/job01/index.php
@@ -0,0 +1,15 @@
+
+
+<!DOCTYPE html>
+	<html>
+	<head>
+		<title>js</title>
+		<script type="text/javascript">
+			alert("Hello javascript");
+		</script>
+	</head>
+	<body>
+	
+	</body>
+	</html>
+</DOCTYPE>
