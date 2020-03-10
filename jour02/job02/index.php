559e426986e7678cc4c2c1dfe3eaa596fb1155e7
olivier-crozet <olivier.crozet@laplateforme.io>
2020-03-10 17:23:07 +0100

    job 02 ok fait en toggle plein d'autre possibilité a revoir plus tard

---
+++ jour02/job02/index.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>index</title>
+	<script type="text/javascript"src="script"></script>
+<style type="text/css">
+	.blue{
+display: block;
+	}
+
+	.masquer{
+		display: none;
+		color: red;
+	}
+</style>
+</head>
+<body>
+<button id="button" onclick="showide()"> buton </button>
+	<article class="blue">
+			<p>L'important n'est pas la chute, mais l'atterrissage.</p>	
+	</article>
+</body>
+</html>
---
+++ jour02/job02/script.js
@@ -0,0 +1,13 @@
+
+
+
+function showide()
+{
+	///////////////////////SELECTION DE LA ZONE ARTICLE////////////
+	let articolo = document.querySelector('.blue');
+		
+	/////////////////:SUPRIME ET ACTIVE AUTOMATIQUEMENT LES CLASS ASSOSIER/////////////	
+	articolo.classList.toggle('masquer');
+
+
+}
