f9adcffabd02bb31489cbe38fb7ae6eb41062a40
olivier-crozet <olivier.crozet@laplateforme.io>
2020-03-10 13:02:00 +0100

    job 01 jai fait mais jai pas compris se que j'ai fait vive le copier coller, pk metre une class a bouton? inner html a quoi sa sert de metre sa dans la console?? j'ai pas le prés requis des fonctions bocoup de travail reste a faire pour moi
    jute pour infos:
    a l'avenir sa peut etre bien de verifier qu'on est assimiler les fonctions !!
    je dis mon resenti sans juger votre travail je me permetré pas j'ai pas votre niveau :)

---
+++ jour02/job01/index.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>index</title>
+	<script type="text/javascript"src="script"></script>
+
+</head>
+<body>
+<button class="button" onclick="citation()">buton</button>
+	<article id="citation">
+				La vie a beaucoup plus d’imagination que nous
+	</article>
+</body>
+</html>
---
+++ jour02/job01/script.js
@@ -0,0 +1,11 @@
+
+function citation()
+{
+	//var azstrefxwbfdwxgxwvxs = document.getElementByid("citation").textContent;
+	//document.getElementById('batard').
+	 let citation = document.getElementById("citation");
+	console.log(citation.innerText.trim());
+}
+
+  /////////////////rien compris ////////////////////////////////
+	
