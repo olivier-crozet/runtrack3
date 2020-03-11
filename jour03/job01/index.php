b85f47cd78878303dfa462bfbd7361f2b58b8dd1
olivier-crozet <olivier.crozet@laplateforme.io>
2020-03-11 11:50:47 +0100

    job 01 ok merci pascal tu ma fait tilté en 1minute en passent me voir a la table j'ai bien compris cette exo du coup merci :) :) :) :)

---
+++ jour03/job01/index.php
@@ -0,0 +1,18 @@
+
+	<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cours jQuery</title>
+        <meta charset="utf-8">
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    </head>
+    <body>
+        
+        <button class="button">click pour mater ;)</button>
+        <h1>le $ est un bon élément
+monétaire. Le # de twitter est une bonne I D.</h1>
+        <button id="cacher">cacher le titre</button>
+    </body>
+</html>
+<script type="text/javascript"src="script"></script>
---
+++ jour03/job01/script.js
@@ -0,0 +1,16 @@
+
+//var butondemerde1 = document.getElementById('button');
+
+    $(document).ready(function(){ $("h1").hide() });
+     $(".button").click(function(){
+        $("h1").show();
+          
+        });
+    
+    $("#cacher").click(function(){
+        $("h1").hide(5000, "linear", function(){
+   
+    });
+
+});
+
